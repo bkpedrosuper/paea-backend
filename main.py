3e9bc4f89a2c4535e2e1456531770d4fb5d716be
Pedro Serpa <pedro.serpa@smi.eng.br>
2024-06-29 19:22:22 -0300

    Updating app

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from fastapi import FastAPI
 from pymongo.mongo_client import MongoClient
-import os
 from dotenv import load_dotenv
 from routes.answer import router as answer_router
 from routes.form import router as form_router
 load_dotenv()
 
 app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
 
 app.include_router(answer_router)
 app.include_router(form_router)
